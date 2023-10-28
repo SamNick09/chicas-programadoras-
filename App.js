af28ae2c579853dfeec0cdfe20760529f44b5884
storatus <sebastian.glahn89@web.de>
2023-10-28 17:58:44 -0400

    cambiosConIconos

--- App.js
+++ App.js
@@ -2,6 +2,10 @@ import { StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
+import AntDesign from "@expo/vector-icons/AntDesign";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import FeatherIcons from "@expo/vector-icons/Feather";
+
 import FirstScreen from "./screens/First";
 import SecondScreen from "./screens/Second";
 import ThirdScreen from "./screens/Third";
@@ -12,11 +16,46 @@ const Tab = createBottomTabNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="FirstScreen" component={FirstScreen} />
-        <Tab.Screen name="SecondScreen" component={SecondScreen} />
-        <Tab.Screen name="ThirdScreen" component={ThirdScreen} />
-        <Tab.Screen name="FourthScreen" component={FourthScreen} />
+      <Tab.Navigator
+        screenOptions={{
+          tabBarShowLabel: false, // Aqui pueden cambiar si hay iconos o no
+        }}
+      >
+        <Tab.Screen
+          options={{
+            tabBarIcon: ({ focused }) => <AntDesign name="home" size={32} />,
+          }}
+          name="FirstScreen"
+          component={FirstScreen}
+        />
+
+        <Tab.Screen
+          options={{
+            tabBarIcon: ({ focused }) => <Ionicons name="menu" size={32} />,
+          }}
+          name="SecondScreen"
+          component={SecondScreen}
+        />
+
+        <Tab.Screen
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <FeatherIcons name="map-pin" size={32} />
+            ),
+          }}
+          name="ThirdScreen"
+          component={ThirdScreen}
+        />
+
+        <Tab.Screen
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <Ionicons name="settings-outline" size={32} />
+            ),
+          }}
+          name="FourthScreen"
+          component={FourthScreen}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
