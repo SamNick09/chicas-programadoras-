87cdb113db67fe155eb91f6f0cd4f497a04b4d33
storatus <sebastian.glahn89@web.de>
2023-11-04 17:32:46 -0400

    guardar

--- screens/First/index.js
+++ screens/First/index.js
@@ -1,9 +1,11 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Dimensions } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 const FirstScreen = () => {
   const espacios = useSafeAreaInsets();
+  const height = Dimensions.get("window").height;
+  const dimensionContenedor = height / 2.3;
 
   return (
     <View style={{ flex: 1, paddingTop: espacios.top }}>
@@ -12,6 +14,7 @@ const FirstScreen = () => {
           justifyContent: "center",
           alignItems: "center",
           gap: 7,
+          marginTop: 40,
         }}
       >
         <Text style={{ fontSize: 30, color: "#FF6666", fontWeight: "600" }}>
@@ -20,6 +23,23 @@ const FirstScreen = () => {
         <Text style={{ fontSize: 20, fontWeight: "500" }}>
           Encuentra y monitorea tus viajes
         </Text>
+      </View>
+      <View
+        style={{
+          justifyContent: "center",
+          alignItems: "center",
+          height: dimensionContenedor,
+          backgroundColor: "gray",
+        }}
+      >
+        <View
+          style={{
+            width: "80%",
+            height: "80%",
+            backgroundColor: "#FF8989",
+            borderRadius: 24,
+          }}
+        ></View>
       </View>
     </View>
   );
