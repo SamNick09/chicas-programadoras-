24ee8ea607c705508990ccbd915e7eb39bc8b65d
storatus <sebastian.glahn89@web.de>
2023-11-11 15:49:35 -0400

    cambio en tercera

--- screens/Third/index.js
+++ screens/Third/index.js
@@ -3,8 +3,29 @@ import { View, Text } from "react-native";
 
 const ThirdScreen = () => {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+    <View style={{ flex: 1 }}>
       <Text style={{ fontWeight: "700", fontSize: 20 }}>Tercer Screen</Text>
+      <View
+        style={{
+          flex: 3,
+          backgroundColor: "red",
+          padding: 10,
+        }}
+      >
+        <View style={{ backgroundColor: "green", flex: 1 }} />
+        <View style={{ backgroundColor: "yellow", flex: 2 }} />
+      </View>
+
+      <View
+        style={{
+          flex: 7,
+          backgroundColor: "blue",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text style={{ color: "white", fontSize: 20 }}>Flex 2</Text>
+      </View>
     </View>
   );
 };
